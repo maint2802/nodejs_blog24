19fedfeac61b6740b55f2587f93f8ae6b076b71b
MaiNT <mai28022000@gmail.com>
2024-10-24 10:57:23 +0700

    express-handlebars

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./resourse/views"));
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home Page" });
+});
+
+app.get("/news", (req, res) => {
+  res.render("news", { title: "Handlebars" });
+});
+
+app.listen(port, () => {
+  console.log("listen tor port: " + port);
+});
+
+// app.use(morgan("combined"));
