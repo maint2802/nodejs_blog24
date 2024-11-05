4ca0b69ff1c933045cd34961b22fe1a205a2a501
MaiNT <mai28022000@gmail.com>
2024-11-05 16:44:08 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
-const fs = require('fs');
-const path = require('path');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
-const routes = require('./routes');
+const routes = require("./routes");
 
 app.engine(
-   'hbs',
-   handlebars.engine({
-      extname: 'hbs',
-   }),
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './resourse/views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./resourse/views"));
 
 app.use(express.urlencoded({ extended: true })); //handle data from html/form
 app.use(express.json()); //handle data from javascript code
 
 routes(app);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
-   console.log('listen tor port: ' + port);
+  console.log("listen tor port: " + port);
 });
 
 // app.use(morgan("combined"));
 const obj = {
-   name: 20,
-   age: 30,
-   gender: 'female',
+  name: 20,
+  age: 30,
+  gender: "female",
 };
